fbd5ebf54a0b72fd1e71f1d08f2b5bbca01a654e
Rik <rik@rkvl.org>
2025-02-17 12:06:46 +0100

    Add scripts

---
+++ src/index.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Channel = exports.MapsUntold = void 0;
+const channel_1 = require("./channel");
+Object.defineProperty(exports, "Channel", { enumerable: true, get: function () { return channel_1.Channel; } });
+class MapsUntold {
+    constructor(config) {
+        // TODO
+    }
+    async getChannel(channelSlug) {
+        // TODO
+    }
+    async getParentCategories(categoryId) {
+        // TODO
+    }
+    async getLocationAutocomplete(query) {
+        // TODO
+    }
+}
+exports.MapsUntold = MapsUntold;
